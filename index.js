0417a6fc5059a43409c72cb378047e3506589a80
Evan Guirino <etg624@gmail.com>
2018-12-07 13:09:14 -0800

    idk

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 // Implement the following features which will require a more complex store object:
 // User can press a switch/checkbox to toggle between displaying all items or displaying only items that are unchecked
-// User can type in a search term and the displayed list will be filtered by item names only containing that search term
+// User can type in a search term and the di`splayed list will be filtered by item names only containing that search term
 // User can edit the title of an item
 
 
